34ef435303d811c16e98b7cf84e37a6b7443a3ba
Lars Hartmann <lars@chaotika.org>
2010-10-06 15:56:56 +0200

    initial commit

---
+++ include/machine.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2010 Lars Hartmann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    1. Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *      
+ *    2. Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* This file describes the interface between pfree and a
+ * machine-dependent module. This header is not machine dependent and
+ * doesn't need to be changed for any module */
+
+#include <stdint.h>
+
+/* this struct is filled by machine_init */
+typedef struct pf_meminfo {
+     int64_t mem_total;         /* bytes of total memory */
+     int64_t mem_used;          /* bytes of used memory */
+     int64_t mem_free;          /* bytes of free memory */
+     int64_t mem_shared;        /* bytes of shared memory */
+     int64_t mem_buffers;       /* bytes of buffers */
+     int64_t mem_cached;        /* bytes of caches */
+     int64_t swap_total;        /* bytes of total swap */
+     int64_t swap_used;         /* bytes of used swap */
+     int64_t swap_free;         /* bytes of free swap */
+} pf_meminfo_t;
+
+/* this is the actual interface to the machine dependent code, this
+ * function is to be implemented as part of porting this program. It's
+ * sole purpose is to fill out the fields in the struct so pfree can
+ * use em. */
+int
+machine_init(pf_meminfo_t *ret);
